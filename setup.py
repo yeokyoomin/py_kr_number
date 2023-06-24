c96efa2bd276d0a67aceb775977e870c65ff4b26
yeokyoomin <62054933+yeokyoomin@users.noreply.github.com>
2023-06-24 16:13:46 +0900

    important_files
    upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='py-kr-number',
+    version='0.0.1',
+    description='Simple Korean unit conversion module',
+    url='https://github.com/yeokyoomin/py-kr-number.git',
+    author='yeokyoomin',
+    author_email='msdhgoom@gmail.com',
+    license='yeokyoomin',
+    packages=['py-kr-number'],
+    zip_safe=False
+)
